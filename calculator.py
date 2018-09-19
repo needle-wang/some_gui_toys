7c1bfe38e6647d22ca650002fb0afb4d587ed2de
needle <needlewang2011@gmail.com>
2018-09-19 14:08:47 +0800

    fix bug

--- calculator.py
+++ calculator.py
@@ -1,11 +1,18 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # encoding: utf-8
-#基本无bug, 只是第二次计算时要手动把第一次的结果清除
-#by needle wang
-#2014年 07月 05日 星期六 16:19:33 CST
+# 基本无bug, 只是第二次计算时要手动把第一次的结果清除
+# by needle wang
+# 2014年 07月 05日 星期六 16:19:33 CST
 
-from Tkinter import Tk, W, E, RIGHT, StringVar
-from ttk import Frame, Button, Label, Style, Entry
+import sys
+if sys.version_info.major == 3:
+  from tkinter import Tk, W, E, RIGHT, StringVar, DISABLED
+  from tkinter.ttk import Frame, Button, Label, Style, Entry
+  # from tkinter.messagebox import showwarning
+else:
+  from Tkinter import Tk, W, E, RIGHT, StringVar, DISABLED
+  from ttk import Frame, Button, Label, Style, Entry
+  from tkMessageBox import showwarning
 
 
 class Example(Frame):
@@ -13,37 +20,40 @@ class Example(Frame):
     CHARS = '1234567890.+-*/()'
 
     def __init__(self, parent):
-        Frame.__init__(self, parent)   
+        Frame.__init__(self, parent)
 
         self.parent = parent
 
-        #在文本区域显示的表达式字符串
-        self.eval_buff=StringVar()
+        # 在文本区域显示的表达式字符串
+        self.eval_buff = StringVar(value=0)
 
         self.initUI()
 
     def initUI(self):
 
+        if sys.version_info.major == 2:
+          showwarning(title = '注意', message = '使用的python版本是python2')
+
         self.parent.title("简单计算器 by needle wang")
-        Style().configure("TButton", padding=(0,5,0,5), font='serif 10')
+        Style().configure("TButton", padding=(0, 5, 0, 5), font='serif 10')
 
         for i in range(4):
-            #列属性配置
+            # 列属性配置
             self.columnconfigure(i, pad=3)
-            #行属性配置
+            # 行属性配置
             self.rowconfigure(i, pad=3)
-            
+
         self.rowconfigure(4, pad=3)
 
-        #一行文本域
+        # 一行文本域
         entry = Entry(self, textvariable=self.eval_buff)
-        entry.grid(row=0, columnspan=4, sticky=W+E)
+        entry.grid(row=0, columnspan=4, sticky=W + E)
 
         cls = Button(self, text="复位(c)", command=self.reset_entry)
         cls.grid(row=1, column=0)
         bck = Button(self, text="退格(back)", command=self.remove_last_one_char)
         bck.grid(row=1, column=1)
-        lbl = Button(self, text="占位符", command=self.focus_set)
+        lbl = Button(self, text="占位符", command=self.focus_set, state=DISABLED)
         lbl.grid(row=1, column=2)
         clo = Button(self, text="退出(q)", command=self.quit)
         clo.grid(row=1, column=3)
@@ -84,65 +94,75 @@ class Example(Frame):
         pls = Button(self, text="+", command=lambda: self.setNum("+"))
         pls.grid(row=5, column=3)
 
-        #获得焦点才能响应按键
+        # 获得焦点才能响应按键
         self.focus_set()
         self.bind("<Key>", self.onPress)
 
         self.pack()
 
-        #self.centerwindow()
+        # self.centerwindow()
 
     def getResult(self):
         try:
             expression = self.eval_buff.get()
-            #如果表达示存在 且 不只是一个点
+            # 如果表达示存在 且 不只是一个点
             if expression and expression != '.':
                 result = eval(expression)
                 self.eval_buff.set(result)
-        except Exception, e:
+        except Exception as e:
             self.eval_buff.set(e)
 
-        #将焦点重设为主frame (Example)
+        # 将焦点重设为主frame (Example)
         self.focus_set()
 
     def setNum(self, button_num):
         '''设置显示区域的表达示'''
         if button_num == '.' and '.' in self.eval_buff.get() :
             return
-        self.eval_buff.set(self.eval_buff.get() + button_num)
+        if self.eval_buff.get() == '0':
+            if button_num.isdigit():
+                self.eval_buff.set(button_num)
+        else:
+            if self.eval_buff.get()[-1:] in ('+', '-', '*', '/') and\
+               button_num in ('+', '-', '*', '/'):
+                self.eval_buff.set(self.eval_buff.get()[:-1] + button_num)
+            else:
+                self.eval_buff.set(self.eval_buff.get() + button_num)
 
-        #将焦点重设为主frame (Example)
+        # 将焦点重设为主frame (Example)
         self.focus_set()
 
     def reset_entry(self):
-        self.eval_buff.set('')
+        self.eval_buff.set('0')
 
-        #将焦点重设为主frame (Example)
+        # 将焦点重设为主frame (Example)
         self.focus_set()
-        
+
     def remove_last_one_char(self):
         '''退格, 直接 [0:-1] 这样的切片操作不会出现数组下标越界'''
-        self.eval_buff.set(self.eval_buff.get()[0:-1])
+        if len(self.eval_buff.get()[0:-1]) == 0:
+            self.eval_buff.set(0)
+        else:
+            self.eval_buff.set(self.eval_buff.get()[0:-1])
 
-        #将焦点重设为主frame (Example)
+        # 将焦点重设为主frame (Example)
         self.focus_set()
-
 
     def onPress(self, event):
         '''增加键盘事件'''
-        #非可见字符(长度为0)能通过in判断~
+        # 非可见字符(长度为0)能通过in判断~
         if event.char:
-            #print ord(event.char)
+            # print ord(event.char)
             if event.char == 'q':
                 self.quit()
             if event.char == 'c':
                 self.reset_entry()
                 return
-            #敲退格则回删一个字符
+            # 敲退格则回删一个字符
             if ord(event.char) == 8:
                 self.remove_last_one_char()
                 return
-            #敲回车则计算表达示
+            # 敲回车则计算表达示
             if ord(event.char) == 13 or event.char == '=':
                 self.getResult()
                 return
@@ -151,17 +171,17 @@ class Example(Frame):
 
     def centerwindow(self):
         '''窗口居中'''
-        #窗口大小
+        # 窗口大小
         w = 600
         h = 500
 
-        #屏幕宽高
+        # 屏幕宽高
         sw = self.parent.winfo_screenwidth()
         sh = self.parent.winfo_screenheight()
 
-        x = (sw - w)/2
-        y = (sh - h)/2
-        self.parent.geometry('%dx%d+%d+%d'%(w, h, x, y))
+        x = (sw - w) / 2
+        y = (sh - h) / 2
+        self.parent.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
 
 def main():
@@ -169,11 +189,11 @@ def main():
     '''没调窗口居中，为什么会自动居中……'''
     root = Tk()
     root.resizable(False, False)
-    #大小：长ｘ宽，位置：x+y
-    #root.geometry("250x150+300+300")
+    # 大小：长ｘ宽，位置：x+y
+    # root.geometry("250x150+300+300")
     Example(root)
-    root.mainloop()  
+    root.mainloop()
 
 
 if __name__ == '__main__':
-    main()  
+    main()
